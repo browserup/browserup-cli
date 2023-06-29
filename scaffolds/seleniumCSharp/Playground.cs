c495c837b72051735dbb76a0be13669273216147
HereC <ebeland@gmail.com>
2023-06-29 11:34:41 -0400

    Add C Sharp

---
+++ scaffolds/seleniumCSharp/Playground.cs
@@ -0,0 +1,23 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics;
+
+class Playground
+{
+
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Url = "http://playground.browserup.com";
+
+        string? think = Environment.GetEnvironmentVariable("think_time");
+        int sleepTime = (think != null) ? int.Parse(think) : 10000;
+
+        Thread.Sleep(sleepTime);
+        driver.FindElement(By.LinkText("Web Playground")).Click();
+
+        Thread.Sleep(sleepTime);
+        Trace.Assert(driver.PageSource.Contains("Toys"));
+    }
+}
