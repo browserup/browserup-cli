bd41cf5024731c0fe109e084b49d55451015c49d
HereC <ebeland@gmail.com>
2023-08-07 10:06:50 -0400

    Add comments to scaffold to explain pathing

--- scaffolds/playwrightJs/playwright-js-example.js
+++ scaffolds/playwrightJs/playwright-js-example.js
@@ -1,6 +1,15 @@
 const playwright = require("playwright");
 
 (async () => {
+    // important note: The standard playwright installation instructions will try to download
+    // browsers into every playwright project, for each language.
+    // This is not ideal for BrowserUp, because we want to share the browser binaries across
+    // projects, as the browsers are large.
+    //
+    // So, we install playwright globally, and then we tell playwright where
+    // to find the browsers. For running outside of browserup, you may need to point this
+    // to the location of your browser.
+
     browser = await playwright.chromium.launch({
         executablePath: "/usr/bin/chromium",
         headless: true
